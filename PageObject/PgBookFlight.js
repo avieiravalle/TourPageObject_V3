972ef5f3e11d3255d3b418019e50789ce5034f0e
avieiravalle <avieiravale@gmail.com>
2019-02-24 11:39:58 -0300

    done it

--- PageObject/PgBookFlight.js
+++ PageObject/PgBookFlight.js
@@ -1,13 +1,18 @@
 var BookFlight = function () {
 
+let PassengersNameInput = $('input[name="passFirst0"]');
+let PassengersLastNameInput = $('input[name="passLast0"]');
+let PassengersCardFlag = $('select[name="creditCard"]').$('[value="IK"]');
+let PassengersCardNumber = $('input[name="creditnumber"]');
+let SecurePurchase = $('[name="buyFlights"]');
 
 
 
-
-
-
-
-
+PassengersNameInput.sendKeys();
+PassengersLastNameInput.sendKeys();
+PassengersCardFlag.click();
+PassengersCardNumber.sendKeys();
+SecurePurchase.click();
 
     
 };
